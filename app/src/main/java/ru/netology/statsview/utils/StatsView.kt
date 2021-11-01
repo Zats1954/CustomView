1d913be4aebbb32e933bf9bd90cf63abbe8750a3
Alexander <alex.zacepin@gmail.com>
2021-11-01 21:44:25 +0100

    animation 1

--- app/src/main/java/ru/netology/statsview/utils/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/utils/StatsView.kt
@@ -1,26 +1,34 @@
 package ru.netology.statsview.utils
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.BounceInterpolator
 import androidx.core.content.withStyledAttributes
-import androidx.core.graphics.PaintCompat
 import ru.netology.statsview.R
 import kotlin.math.min
 import kotlin.random.Random
 
 class StatsView @JvmOverloads constructor(context: Context,
 attrs: AttributeSet? = null) : View(context,attrs) {
+
     private var center = PointF(0F, 0F)
     private var radius = 0F
     private var linedWidth = 0
+    private var colors = emptyList<Int>()
+    private var progress = 0F
+    private var progress1 = 0F
+    private var animator: Animator? = null
+    var prog:FloatArray? = null
+
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply{
         style = Paint.Style.STROKE
         strokeCap = Paint.Cap.ROUND
         strokeJoin = Paint.Join.ROUND
     }
-
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply{
         style = Paint.Style.STROKE
@@ -30,24 +38,52 @@ attrs: AttributeSet? = null) : View(context,attrs) {
     var data: List<Float> = emptyList()
        set(value) {
           field = value
-          invalidate()
+           update()
     }
 
-private var colors = emptyList<Int>()
+    private fun update() {
+        animator?.let{
+            it.removeAllListeners()
+            it.cancel()
+        }
+        val prog1 = FloatArray(data.size)
+        animator = ValueAnimator.ofFloat(0F, 4F).apply {
+            addUpdateListener {
+                progress = it.animatedValue as Float
+                for(i in 0 ..  prog1.size -1 ){
+                    if(progress < i){
+                        prog1[i]=0F}
+                    else{
+                        if(progress > i + 1)
+                            prog1[i]=1F
+                        else
+                         prog1[i] = progress - i}
+                        println("${progress} -> ${prog1[i]} ${i}")
+                }
+                prog = prog1
+                invalidate()
+            }
+            duration = 5_000
+            start()
+        }
+
+    }
+
 init{
     context.withStyledAttributes(attrs, R.styleable.StatsView){
         textPaint.textSize = getDimension(R.styleable.StatsView_textSize,
             AndroidUtils.dp(context, 40F).toFloat())
+
         linedWidth = getDimensionPixelSize(R.styleable.StatsView_lineWidth,
-            AndroidUtils.dp(context, 15F))
+            AndroidUtils.dp(context, 10F))
 
         paint.strokeWidth = linedWidth.toFloat()
+
         colors = listOf(
             getColor(R.styleable.StatsView_color1, getRandomColor()),
             getColor(R.styleable.StatsView_color2, getRandomColor()),
             getColor(R.styleable.StatsView_color3, getRandomColor()),
             )
-
     }
 }
 
@@ -67,23 +103,24 @@ init{
             return
         var startAngle = -90F
         var addAngle = startAngle
+
         data.forEachIndexed { index,item ->
             val angle = item * 360F
 //  **********************   Задача Not Filled ********************
-//            if(index.equals(3)){
+//            if(index.equals(data.lastIndex)){
 //                paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.DST_OVER))
 //            } else{
 //                paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC))
 //            }
             paint.color = colors.getOrElse(index) {getRandomColor()}
-            canvas.drawArc(circle, startAngle, angle,false,paint)
-            if(index.equals(0)){
+            canvas.drawArc(circle, startAngle, angle * (prog?.let{it[index]} ?: 0F), false, paint)
+            if(index == 0){
                 addAngle = angle/2
             }
             startAngle += angle
         }
         paint.color = colors.getOrElse(0) {getRandomColor()}
-        canvas.drawArc(circle, startAngle, addAngle,false,paint)
+        canvas.drawArc(circle, startAngle, addAngle , false, paint)
         canvas.drawText(
             "%.2f%%".format(data.sum()*100),
             center.x,
@@ -91,6 +128,5 @@ init{
             textPaint
         )
     }
-
     private fun getRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 }
